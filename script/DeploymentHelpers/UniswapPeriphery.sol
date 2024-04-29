52f64104516b149a9984b55ababe3a971f9bd2c6
Lily Johnson <lillianjanjohnson@gmail.com>
2024-04-29 15:56:02 -0400

    fix submodules

--- script/DeploymentHelpers/UniswapPeriphery.sol
+++ script/DeploymentHelpers/UniswapPeriphery.sol
@@ -1,3 +1,3 @@
 // SPDX-License-Identifier: UNLICENSED
-import {NonfungiblePositionManager} from "../../lib/v3-periphery/contracts/NonfungiblePositionManager.sol";
-import {SwapRouter} from "../../lib/v3-periphery/contracts/SwapRouter.sol";
+import {NonfungiblePositionManager} from "../../lib/v3-periphery-fixed/contracts/NonfungiblePositionManager.sol";
+import {SwapRouter} from "../../lib/v3-periphery-fixed/contracts/SwapRouter.sol";
